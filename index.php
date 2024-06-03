d8ea2dda1ea0da50776b3577f7e2dadd764c406d
edanielmoram@gmail.com <edanielmoram@gmail.com>
2024-06-02 23:02:24 -0600

    seccion

--- index.php
+++ index.php
@@ -1,2 +1,67 @@
-dfghmjhyt4r3ewsxc
-asdfghgtrewsxcvb<?phpsdfghgdsxcdvv<?php
+<?php
+     ini_set('display_errors', '1');
+     ini_set('display_startup_errors', '1');
+     error_reporting(E_ALL);
+
+require '../../modelos/Cliente.php';
+
+// VALIDAR INFORMACION
+$_POST['cli_id'] = filter_var($_POST['cli_id'], FILTER_VALIDATE_INT);
+$_POST['cli_nombre'] = htmlspecialchars($_POST['cli_nombre']);
+$_POST['cli_apellido'] = htmlspecialchars($_POST['cli_apellido']);
+$_POST['cli_nit'] = filter_var($_POST['cli_nit'], FILTER_VALIDATE_INT);
+$_POST['cli_telefono'] = filter_var($_POST['cli_telefono'], FILTER_VALIDATE_INT);
+
+
+if ($_POST['cli_nombre'] == '' || $_POST['cli_apellido'] == '' || $_POST['cli_nit'] < 0 || $_POST['cli_telefono'] < 0) {
+    // ALERTA PARA VALIDAR DATOS
+    $resultado = [
+        'mensaje' => 'DEBE VALIDAR LOS DATOS',
+        'codigo' => 2
+    ];
+} else {
+    try {
+        // REALIZAR CONSULTA
+        $cliente = new Cliente($_POST);
+
+        
+        $modificar = $cliente->modificar();
+
+        $resultado = [
+            'mensaje' => 'CLIENTE MODIFICADO CORRECTAMENTE',
+            'codigo' => 1
+        ];
+    } catch (PDOException $pe) {
+        $resultado = [
+            'mensaje' => 'OCURRIO UN ERROR MODIFICANDO EL REGISTRO A LA BD',
+            'detalle' => $pe->getMessage(),
+            'codigo' => 0
+        ];
+    } catch (Exception $e) {
+        $resultado = [
+            'mensaje' => 'OCURRIO UN ERROR EN LA EJECUCIÓN',
+            'detalle' => $e->getMessage(),
+            'codigo' => 0
+        ];
+    }
+}
+
+
+$alertas = ['danger', 'success', 'warning'];
+
+
+include_once '../../vistas/templates/header.php'; ?>
+
+<div class="row justify-content-center">
+    <div class="col-lg-6 alert alert-<?= $alertas[$resultado['codigo']] ?>" role="alert">
+        <?= $resultado['mensaje'] ?>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-lg-6">
+        <a href="../../controladores/clientes/buscar.php" class="btn btn-primary w-100">Regresar</a>
+    </div>
+</div>
+
+
+<?php include_once '../../vistas/templates/footer.php'; ?>
